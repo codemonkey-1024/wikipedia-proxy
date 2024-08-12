1bbb98efefe0d6587fb33bc9ea96dc6371b3bf7d
codemonkey <56342978+codemonkey-1024@users.noreply.github.com>
2024-08-12 22:58:48 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    url.host = "en.wikipedia.org";
+    url.host = "www.wikidata.org";
  
     console.log('Accessing URL:', url.href);
     return fetch(url, {
