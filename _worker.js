cd52a5f884964d1c5d9597c34d7f84891741ce3d
codemonkey <56342978+codemonkey-1024@users.noreply.github.com>
2024-09-11 12:06:25 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    url.host = "en.wikipeida.org";
+    url.host = "en.wikipedia.org";
  
     console.log('Accessing URL:', url.href);
     return fetch(url, {
